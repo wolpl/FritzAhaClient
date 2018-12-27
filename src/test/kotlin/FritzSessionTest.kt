2331273c12151e9040e6c745917f947aa854d6b2
wolpl <wolpl-dev@outlook.com>
2018-12-27 10:48:19 +0100

    Add testing comment

--- src/test/kotlin/FritzSessionTest.kt
+++ src/test/kotlin/FritzSessionTest.kt
@@ -19,6 +19,17 @@
  *
  * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
+/*
+ * FritzAhaClient is a Kotlin/JVM client for accessing the AVM Fritzbox AHA-HTTP Interface.
+ * Copyright (C) 2018  wolpl (github.com/wolpl)
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 import com.github.wolpl.fritzahaclient.FritzSession
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -27,6 +38,8 @@ import java.io.File
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class FritzSessionTest {
+
+    //Create a file /src/test/resources/FritzboxCredentials to test with a real Fritzbox. First line is the username, second line the password
     private val username = File(FritzSessionTest::class.java.getResource("FritzboxCredentials").file).readLines()[0]
     private val password = File(FritzSessionTest::class.java.getResource("FritzboxCredentials").file).readLines()[1]
 
